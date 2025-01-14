ec90830db47f35ce58c293b7783a90fef885b1f5
NtwariMikeCK <k.ntwari@alustudent.com>
2025-01-14 10:48:48 -0800

    done

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" This file finds the steps it will take us"""
+
+def minOperations(n):
+    """get the minimum operations"""
+    if n <= 1:
+        return 0
+    operations = 0
+    divisor = 2
+
+    while divisor * divisor <= n:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
